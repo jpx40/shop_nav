ced5887b4abf6e8728d80280d468f8106672b7b7
jonas <jonaspetiz@proton.me>
2024-07-25 13:26:03 +0200

    1

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/a-h/templ"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		templ.Handler(Hello("world")).ServeHTTP(w, r)
+	})
+	http.ListenAndServe(":3000", r)
+}
